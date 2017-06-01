e88a5628b4706e4cde0a510c6de3477505756607
Fabio Carvalho <fabaoanalista@gmail.com>
2017-06-01 18:25:40 -0300

    Alterações e inclusões exercicios

---
+++ 3_Js/jquery/funcoes.js
@@ -0,0 +1,10 @@
+// Sintaxe:
+//$(seletor).acao()
+//versao simplificada Document.ready ()
+//$(function(){
+//});
+
+$(document).ready(function(){
+  //todo código jquery-3
+  console.log("Doc pronto para uso");
+});
